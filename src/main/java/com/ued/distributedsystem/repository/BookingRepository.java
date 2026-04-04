81213820236a39a21b6f32f52f9c66ec60118390
Cham2212 <tramtretrauhehe@gmail.com>
2026-04-04 10:38:21 +0700

    Delete src/main directory

--- src/main/java/com/ued/distributedsystem/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ued.distributedsystem.repository;
-
-import com.ued.distributedsystem.model.Booking;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookingRepository extends MongoRepository<Booking, String> {
-}
